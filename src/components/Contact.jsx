59f2fb949dc918c0ce28eaf0c8a5c55a2e06865f
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-06-14 19:36:17 +0200

    Contact design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,26 +9,25 @@ export default () => {
                     <h3 className="section-subheading text-muted"></h3>
                 </div>
                 <form name="contact" method="post" id="contactForm">
+
+                <input type="hidden" name="form-name" value="contact" />
+                    
                     <div className="row align-items-stretch mb-5">
                         <div className="col-md-6">
                             <div className="form-group">
-                                <input className="form-control" name="name" id="name" type="text" placeholder="Your Name *"  />
-                                <p className="help-block text-danger"></p>
+                                <input className="form-control" name="name" type="text" placeholder="Your Name *"  />
                             </div>
                             <div className="form-group">
-                                <input className="form-control" id="email" name="email" type="email" placeholder="Your Email *"  />
-                                <p className="help-block text-danger"></p>
+                                <input className="form-control" name="email" type="email" placeholder="Your Email *"  />
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div className="form-group form-group-textarea mb-md-0">
-                                <textarea className="form-control" id="message" name="message" placeholder="Your Message *" ></textarea>
-                                <p className="help-block text-danger"></p>
+                                <textarea className="form-control" name="message" placeholder="Your Message *" ></textarea>
                             </div>
                         </div>
                     </div>
                     <div className="text-center">
-                        <div id="success"></div>
                         <button className="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
                     </div>
                     
