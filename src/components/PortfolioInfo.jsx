ccc54cff278ea4c8a5fa5b389546d36ee5f122fe
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-06-15 20:24:13 +0200

    Added github and demo button

--- src/components/PortfolioInfo.jsx
+++ src/components/PortfolioInfo.jsx
@@ -13,7 +13,9 @@ export default () => {
                                     <h2 className="text-uppercase">Project Name</h2>
                                     <p className="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
                                     <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
-                                    <button className="btn btn-primary" data-dismiss="modal" type="button"><i className="fas fa-times mr-1"></i>Close Project</button>
+                                    <button className="btn btn-primary" data-dismiss="modal" type="button"><i class="fab fa-github"></i> View Code</button>
+                                    <button className="btn btn-primary" data-dismiss="modal" type="button"><i class="fas fa-external-link-alt"></i> View Demo</button>
+                                    <button className="btn btn-secondary" data-dismiss="modal" type="button"><i className="fas fa-times mr-1"></i> Close </button>
                                 </div>
                             </div>
                         </div>
