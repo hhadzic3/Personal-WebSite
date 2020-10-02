7a96f96a3ebb1770c84e84052fe26a59af860f24
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-10-02 11:44:16 +0200

    About fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,48 +12,48 @@ export default ({history}) => {
                     exceptional websites, applications, and everything in between.</h3>
             </div>
             <ul className="timeline">
-            { history && history.map(( {title , subtitle , description,img} , i) => 
-                ( i !== history.length ? 
+            { history && history.map(( {title , subtitle , description,img} , i) =>            
+                ( i % 2 === 0 ?
                     (
-                        (
-                            i % 2 === 0 ?
-                            (
-                            <li key={title}>
-                                <div className="timeline-image">
-                                    <img className="rounded-circle img-fluid" src={img} alt="" />
-                                </div>
-                                <div className="timeline-panel">
-                                    <div className="timeline-heading">
-                                        <h4>{title}</h4>
-                                        <h4 className="subheading"> {subtitle} </h4>
-                                    </div>
-                                    <div className="timeline-body"><p className="text-muted">{description}</p></div>
-                                </div>
-                            </li>
-                            ) :
-                            (
-                            <li key={title} className="timeline-inverted">
-                                <div className="timeline-image">
-                                    <img className="rounded-circle img-fluid" src={img} alt="" />
-                                </div>
-                                <div className="timeline-panel">
-                                    <div className="timeline-heading">
-                                        <h4>{title}</h4>
-                                        <h4 className="subheading"> {subtitle} </h4>
-                                    </div>
-                                    <div className="timeline-body"><p className="text-muted">{description}</p></div>
-                                </div>
-                            </li>
-                            )
-                        )
-                    ) : (
-                    <li key={title} className="timeline-inverted">
+                    <li key={title}>
                         <div className="timeline-image">
-                            <h4>Be Part<br />Of My/Our<br />Story!</h4>
+                            <img className="rounded-circle img-fluid" src={img} alt="" />
+                        </div>
+                        <div className="timeline-panel">
+                            <div className="timeline-heading">
+                                <h4>{title}</h4>
+                                <h4 className="subheading"> {subtitle} </h4>
+                            </div>
+                            <div className="timeline-body"><p className="text-muted">{description}</p></div>
                         </div>
                     </li>
+                    ) :
+                    (
+                        <>
+                    <li key={title} className="timeline-inverted">
+                        <div className="timeline-image">
+                            <img className="rounded-circle img-fluid" src={img} alt="" />
+                        </div>
+                        <div className="timeline-panel">
+                            <div className="timeline-heading">
+                                <h4>{title}</h4>
+                                <h4 className="subheading"> {subtitle} </h4>
+                            </div>
+                            <div className="timeline-body"><p className="text-muted">{description}</p></div>
+                        </div>
+                    </li>
+                    { i === history.length-1 &&
+                        <li key={title} className="timeline-inverted">
+                            <div className="timeline-image">
+                                <h4>Be Part<br />Of My/Our<br />Story!</h4>
+                            </div>
+                        </li>
+                    }
+                    </>
                     )
                 )
+                        
+                  
             )}
             </ul>
         </div>
